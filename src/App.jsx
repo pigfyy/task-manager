09a49d550be115edf947c7a52a5e5995740e2bf3
pigfyy <111782362+pigfyy@users.noreply.github.com>
2023-05-20 12:25:12 -0700

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
-import Sidebar from "@/components/sidebar/Sidebar";
+import BoardList from "@/components/boardList/BoardList";
 import Header from "@/components/header/Header";
-import { useAppStore } from "@/assets/zustand/AppStore";
+import { useAppStore, useBoardListShownStore } from "@/assets/zustand/AppStore";
+import { ReactComponent as LogoDark } from "@/assets/icons/logo-dark.svg";
+import { ReactComponent as LogoLight } from "@/assets/icons/logo-light.svg";
 
 export default () => {
   const isDarkMode = useAppStore((state) => state.isDarkMode);
+  const [boardListShown, setBoardListShown] = useBoardListShownStore(
+    (state) => [state.boardListShown, state.setBoardListShown]
+  );
 
   return (
-    <div className={isDarkMode ? "dark" : ""}>
-      <div className="flex w-screen h-screen justify-center items-center bg-neutral-400 dark:bg-neutral-700">
-        <Sidebar />
-        {/* <Header /> */}
+    <div className={`min-h-screen ${isDarkMode ? "dark" : ""}`}>
+      <div className="flex">
+        {boardListShown && (
+          <div className="hidden min-h-screen flex-shrink-0 flex-col bg-neutral-100 dark:bg-neutral-800 md:flex">
+            <div className="mx-6 mb-7 mt-5">
+              {isDarkMode ? <LogoLight /> : <LogoDark />}
+            </div>
+            <BoardList />
+          </div>
+        )}
+        <div className="flex min-h-screen w-full flex-col bg-neutral-150 dark:bg-neutral-700">
+          <Header />
+        </div>
       </div>
     </div>
   );
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,21 +3,91 @@ import { ReactComponent as IconChevronUp } from "@/assets/icons/icon-chevron-up.
 import { ReactComponent as IconChevronDown } from "@/assets/icons/icon-chevron-down.svg";
 import { ReactComponent as IconMobileAddTask } from "@/assets/icons/icon-add-task-mobile.svg";
 import { ReactComponent as IconVerticalEllipsis } from "@/assets/icons/icon-vertical-ellipsis.svg";
+import { ReactComponent as LogoDark } from "@/assets/icons/logo-dark.svg";
+import { ReactComponent as LogoLight } from "@/assets/icons/logo-light.svg";
+
+import BoardList from "@/components/boardList/BoardList";
+import {
+  useFloating,
+  autoUpdate,
+  offset,
+  flip,
+  shift,
+  useClick,
+  useDismiss,
+  useRole,
+  useInteractions,
+  FloatingFocusManager,
+} from "@floating-ui/react";
+import { useBoardListShownStore } from "@/assets/zustand/AppStore";
 
 export default () => {
+  const [boardListShown, setBoardListShown] = useBoardListShownStore(
+    (state) => [state.boardListShown, state.setBoardListShown]
+  );
+
+  const { refs, floatingStyles, context } = useFloating({
+    open: boardListShown,
+    onOpenChange: setBoardListShown,
+    middleware: [offset(45), flip(), shift()],
+    whileElementsMounted: autoUpdate,
+  });
+
+  const click = useClick(context);
+  const dismiss = useDismiss(context);
+  const role = useRole(context);
+
+  const { getReferenceProps, getFloatingProps } = useInteractions([
+    click,
+    dismiss,
+    role,
+  ]);
+
   return (
-    <div className="w-full bg-neutral-100 flex items-center px-4 gap-4 py-5 dark:bg-neutral-800">
-      <LogoMobile />
-      <div className="flex gap-2 items-center">
-        <p className="text-h-l dark:text-neutral-100">Platform Launch</p>
-        <IconChevronDown />
+    <div className="flex">
+      <div className="flex items-center bg-neutral-100 pl-4 dark:bg-neutral-800 md:border-r-[1px] md:border-r-neutral-700 md:px-6">
+        <div className="md:hidden">
+          <LogoMobile />
+        </div>
+        {!boardListShown && (
+          <div className="hidden md:block">
+            <div>
+              <LogoLight />
+            </div>
+            <div>
+              <LogoDark />
+            </div>
+          </div>
+        )}
       </div>
-      <button className="rounded-3xl bg-primary-400 px-[18px] py-[10px] ml-auto">
-        <IconMobileAddTask />
-      </button>
-      <button>
-        <IconVerticalEllipsis />
-      </button>
+      <div className="flex flex-grow items-center gap-4 bg-neutral-100 px-4 py-5 dark:bg-neutral-800 md:px-6">
+        <div className="flex gap-2">
+          <p className="text-h-l dark:text-neutral-100">Platform Launch</p>
+          <button
+            className="block md:hidden"
+            ref={refs.setReference}
+            {...getReferenceProps()}
+          >
+            {boardListShown ? <IconChevronUp /> : <IconChevronDown />}
+          </button>
+          <FloatingFocusManager context={context} modal={false}>
+            <div
+              ref={refs.setFloating}
+              style={floatingStyles}
+              {...getFloatingProps()}
+              className="block md:hidden"
+            >
+              {boardListShown && <BoardList />}
+            </div>
+          </FloatingFocusManager>
+        </div>
+        <button className="ml-auto rounded-3xl bg-primary-400 px-[18px] py-[10px]">
+          <IconMobileAddTask />
+        </button>
+        <button>
+          <IconVerticalEllipsis />
+        </button>
+      </div>
     </div>
   );
 };
